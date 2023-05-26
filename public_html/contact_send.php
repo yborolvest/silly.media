3702b34336c5f734c78b0796fa5e10dd511174b6
Yvo Rolefes <yvo@earlyharvest.nl>
2023-05-26 15:37:46 +0200

    dawd

--- public_html/contact_send.php
+++ public_html/contact_send.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sending message...</title>
 </head>
 <body>
     <p>...</p>
@@ -23,7 +23,7 @@ if (isset($_POST['email']) && $_POST['email']) {
 
 }
 
-$token = $_POST['token'];
+$token = $_POST['g-recaptcha-response'];
 $action = $_POST['action'];
 
 // call curl to POST request
