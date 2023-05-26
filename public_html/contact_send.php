13440e75f062159868c61996234a5baed9b886a1
Yvo Rolefes <yvo@earlyharvest.nl>
2023-05-26 15:17:09 +0200

    Update contact_send.php

--- public_html/contact_send.php
+++ public_html/contact_send.php
@@ -1,53 +1,48 @@
 <?php
-
-
 $privatekey = "6LdCwTwmAAAAAG0lI620dGAncSBxc8RwnCFEl5aK";
-  $resp = recaptcha_check_answer ($privatekey,
+$resp = recaptcha_check_answer($privatekey,
                                 $_SERVER["REMOTE_ADDR"],
                                 $_POST["recaptcha_challenge_field"],
                                 $_POST["recaptcha_response_field"]);
 
-  if (!$resp->is_valid) {
-    // What happens when the CAPTCHA was entered incorrectly
+if (!$resp->is_valid) {
     die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
          "(reCAPTCHA said: " . $resp->error . ")");
-  } else {
+} else {
     $errors = '';
-$myemail = 'ybo@yborolvest.nl';
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
+    $myemail = 'ybo@yborolvest.nl';
+    if(empty($_POST['name'])  || 
+    empty($_POST['email']) || 
+    empty($_POST['message']))
+    {
+        $errors .= "\n Error: all fields are required";
+    }
+
+    $name = $_POST['name']; 
+    $email_address = $_POST['email']; 
+    $message = $_POST['message']; 
+
+    if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email_address))
+    {
+        $errors .= "\n Error: Invalid email address";
+    }
+
+    if( empty($errors))
+    {
+    $to = $myemail;
+    $email_subject = "Contact form submission: $name";
+    $email_body = "You have received a new message. ".
+    " Here are the details:\n Name: $name \n ".
+    "Email: $email_address\n Message \n $message";
+
+    $headers = "From: $myemail\n";
+    $headers .= "Reply-To: $email_address";
+    mail($to,$email_subject,$email_body,$headers);
+
+
+    header('Location: https://yborolvest.nl/?email=succes');
+
+    }
 }
 
-$name = $_POST['name']; 
-$email_address = $_POST['email']; 
-$message = $_POST['message']; 
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-
-if( empty($errors))
-{
-$to = $myemail;
-$email_subject = "Contact form submission: $name";
-$email_body = "You have received a new message. ".
-" Here are the details:\n Name: $name \n ".
-"Email: $email_address\n Message \n $message";
-
-$headers = "From: $myemail\n";
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-
-
-header('Location: https://yborolvest.nl/?email=succes');
-
-}
-  }
-  ?>
 
